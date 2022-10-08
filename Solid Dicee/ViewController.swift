682cc3776eba79517497deb2e598669c003e9e79
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2022-10-08 18:55:00 +0300

    IBOutlets imageViews and IBAction rollPressed added.

--- Solid Dicee/ViewController.swift
+++ Solid Dicee/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImages = [ UIImage(named: "DiceOne"),
+                       UIImage(named: "DiceTwo"),
+                       UIImage(named: "DiceThree"),
+                       UIImage(named: "DiceFour"),
+                       UIImage(named: "DiceFive"),
+                       UIImage(named: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // Each element of diceArray is an optional variable. The result of randomElement() is also an optional variable
+        // so result need to be unwrapped
+        diceImageView1.image = diceImages.randomElement()!  //as! UIImage(parent) --> unwrapping
+        diceImageView2.image = diceImages.randomElement()!  //as! UIImage(parent) --> unwrapping
+    }
 }
 
